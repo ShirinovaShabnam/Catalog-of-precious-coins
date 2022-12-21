b390d99e8eed345253e7b3a52d9c2633de4c89b3
ShirinovaShabnam <100684990+ShirinovaShabnam@users.noreply.github.com>
2022-12-22 00:56:12 +0400

    Add files via upload

---
+++ server-coins/const.js
@@ -0,0 +1,11 @@
+const DB_CONNECTION = {
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'archive_alexandr'
+}
+
+module.exports = {
+    DB_CONNECTION: DB_CONNECTION 
+}
